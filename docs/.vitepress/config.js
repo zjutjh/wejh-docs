21020326e7f4f90f74ed25ef8822538d5a4f1944
popWheat <49830650+j10ccc@users.noreply.github.com>
2023-07-17 20:19:28 +0800

    chore(ci): deploy on GitHub page

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
+  base: "/wejh-docs/",
   title: "微精弘开发文档",
   description: "docs for wejh weapp development",
   themeConfig: {
